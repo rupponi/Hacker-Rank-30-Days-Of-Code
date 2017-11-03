e7cc5ffb0ea6da8f441950566e0dafed6f247487
Rohan Upponi <rponi70@gmail.com>
2017-11-03 04:15:49 -0700

    Pushing Day 8 of HackerRank 30 Day Challenge: Dictionaries and Maps (Java)

---
+++ Java-Source/Day8.java
@@ -0,0 +1,29 @@
+//DAY 8 OF HACKERRANK 30 DAY CHALLENGE: DICTIONARIES AND MAPS (JAVA)
+//NOTE: The name of the class was changed for organization.
+
+//Complete this code or write your own from scratch
+import java.util.*;
+
+class Solution{
+    public static void main(String []args){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        Map<String, Integer> phoneBook = new HashMap<String, Integer>();
+
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            phoneBook.put(name,phone);
+        }
+        while(in.hasNext()){
+            String s = in.next();
+            if (phoneBook.containsKey(s)) {
+                System.out.printf("%s=%d\n",s,phoneBook.get(s));
+            }
+            else {
+                System.out.println("Not found");
+            }
+        }
+        in.close();
+    }
+}
