a8a85fa923692d9f702e61e6fe701404fc5ea538
Rohan Upponi <rponi70@gmail.com>
2017-11-07 14:53:52 -0700

    Pushing Day 13: Abstract Classes (Java)

---
+++ Java-Source/Day13.java
@@ -0,0 +1,45 @@
+//DAY 13 OF HACKERRANK 30 DAY CHALLENGE: ABSTRACT CLASSES (JAVA)
+//NOTE: Class name was changed for organization.
+
+import java.util.*;
+
+abstract class Book {
+    String title;
+    String author;
+
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    abstract void display();
+}
+
+class MyBook extends Book {
+    int price;
+
+    MyBook(String titleIn, String authorIn, int priceIn) {
+        super(titleIn,authorIn);
+        price = priceIn;
+    }
+
+    void display() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
+}
+
+public class Day13 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        String author = scanner.nextLine();
+        int price = scanner.nextInt();
+        scanner.close();
+
+        Book book = new MyBook(title, author, price);
+        book.display();
+    }
+}
