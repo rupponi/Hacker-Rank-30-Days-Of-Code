8c2375f0a87c2a37d52e64913153e4a7f02bb0b7
Rohan Upponi <rponi70@gmail.com>
2017-11-12 02:15:04 -0700

    Pushing Day 2: Data Types (Python)

---
+++ Python-Source/Day2.py
@@ -0,0 +1,18 @@
+# DAY 2 OF HACKERRANK 30 DAY CHALLENGE: DATA TYPES (PYTHON)
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+iIn = input()
+dIn = input()
+sIn = input()
+
+iIn = int(iIn)
+print(iIn+i)
+
+dIn = float(dIn)
+print(dIn + d)
+
+print(s + sIn)
+
