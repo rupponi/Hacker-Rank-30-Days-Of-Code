618dfe1f52a6c9dff0d12b5c79eba0ba7b71ab40
rupponi <rponi70@gmail.com>
2017-11-14 11:21:12 -0700

    Pushing Day 19: Interfaces (Java)

---
+++ Java-Source/Day19.java
@@ -0,0 +1,41 @@
+//DAY 19 OF HACKERRANK 30 DAY CHALLENGE: INTERFACES (JAVA)
+//NOTE: The Calculator class was renamed Interface Calculator due to
+//      clashing with a Calculator class in a previous exercise.
+
+import java.util.*;
+
+interface AdvancedArithmetic{
+    int divisorSum(int n);
+}
+
+class InterfaceCalculator implements AdvancedArithmetic {
+    int output;
+
+    public InterfaceCalculator() {
+        output = 0;
+    }
+
+    public int divisorSum(int n) {
+        for (int x = 1; x <= 1000; x++) {
+            if (n % x == 0) {
+                output += x;
+            }
+        }
+
+        return output;
+    }
+}
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+
+        AdvancedArithmetic myCalculator = new InterfaceCalculator();
+        int sum = myCalculator.divisorSum(n);
+        System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName() );
+        System.out.println(sum);
+    }
+}
