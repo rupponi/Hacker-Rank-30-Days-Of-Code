3af85626c8988bdc03a40481f0d72e7635ba345b
Rohan Upponi <rponi70@gmail.com>
2017-11-16 02:07:27 -0700

    Pushing Day 21: Generics (Java)

---
+++ Java-Source/Day21.java
@@ -0,0 +1,37 @@
+//DAY 21 OF HACKERRANK 30 DAY CHALLENGE: GENERICS (JAVA)
+
+import java.util.*;
+
+class Printer <T> {
+    void printArray(T[] input) {
+        for (int x = 0; x < input.length; x++) {
+            System.out.println(input[x]);
+        }
+    }
+}
+
+public class Day21 {
+
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Integer[] intArray = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            intArray[i] = scanner.nextInt();
+        }
+
+        n = scanner.nextInt();
+        String[] stringArray = new String[n];
+        for (int i = 0; i < n; i++) {
+            stringArray[i] = scanner.next();
+        }
+
+        Printer<Integer> intPrinter = new Printer<Integer>();
+        Printer<String> stringPrinter = new Printer<String>();
+        intPrinter.printArray( intArray  );
+        stringPrinter.printArray( stringArray );
+        if(Printer.class.getDeclaredMethods().length > 1){
+            System.out.println("The Printer class should only have 1 method named printArray.");
+        }
+    }
+}
