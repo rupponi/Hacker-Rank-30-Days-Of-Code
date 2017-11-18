dc12fff26fc9d3a898788f88f6a8bd1fc41b651f
Rohan Upponi <rponi70@gmail.com>
2017-11-17 18:40:48 -0700

    Pushing Day 22: Binary Search Trees (C++)

---
+++ CPP-Source/Day22.cpp
@@ -0,0 +1,73 @@
+//DAY 22 OF HACKERRANK 30 DAY CHALLENGE: BINARY SEARCH TREES (C++)
+
+#include <iostream>
+#include <cstddef>
+
+using namespace std;
+
+class Node {
+public:
+	int data;
+	Node *left;
+	Node *right;
+	Node(int d) {
+		data = d;
+		left = NULL;
+		right = NULL;
+	}
+};
+class Solution {
+public:
+	Node* insert(Node* root, int data) {
+		if (root == NULL) {
+			return new Node(data);
+		}
+		else {
+			Node* cur;
+			if (data <= root->data) {
+				cur = insert(root->left, data);
+				root->left = cur;
+			}
+			else {
+				cur = insert(root->right, data);
+				root->right = cur;
+			}
+
+			return root;
+		}
+	}
+	int max(int a, int b) {
+		if (a > b) {
+			return a;
+		}
+		else {
+			return b;
+		}
+	}
+
+	int getHeight(Node* root) {
+		if (root == NULL) {
+			return -1;
+		}
+
+		return max(getHeight(root->left), getHeight(root->right)) + 1;
+	}
+}; //End of Solution
+
+int main() {
+	Solution myTree;
+	Node* root = NULL;
+	int t;
+	int data;
+
+	cin >> t;
+
+	while (t-- > 0) {
+		cin >> data;
+		root = myTree.insert(root, data);
+	}
+	int height = myTree.getHeight(root);
+	cout << height;
+
+	return 0;
+}
