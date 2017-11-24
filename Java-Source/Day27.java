ffd6344746405f060f88a39f3b035a304b8f396d
Rohan Upponi <rponi70@gmail.com>
2017-11-23 17:31:35 -0700

    Pushing Day 27: Testing (Java)

---
+++ Java-Source/Day27.java
@@ -0,0 +1,18 @@
+//DAY 27 OF HACKERRANK 30 DAY CHALLENGE: TESTING (JAVA)
+
+public class Day27 {
+
+    public static void main(String[] args) {
+        System.out.println(5);
+        System.out.println(5 + " " + 4);
+        System.out.println(5 + " " + 4 + " " + 3 + " " + -1 + " " + 0);
+        System.out.println(4 + " " + 2);
+        System.out.println(2 + " " + 3 + " " + 0 + " " + -1);
+        System.out.println(5 + " " + 2);
+        System.out.println(4 + " " + 2 + " " + 3 + " " + 0 + " " + 1);
+        System.out.println(5 + " " + 1);
+        System.out.println(5 + " " + 3 + " " + 2 + " " + 9 + " " + 0);
+        System.out.println(3 + " " + 3);
+        System.out.println(0 + " " + -5 + " " + 1);
+    }
+}
