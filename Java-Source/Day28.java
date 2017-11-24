259a7fe0b2006dd99fcfb3a56b986914f951b2d4
Rohan Upponi <rponi70@gmail.com>
2017-11-23 18:16:38 -0700

    Pushing Day 28: RegEx, Patterns, and Intro to Databases (Java)

---
+++ Java-Source/Day28.java
@@ -0,0 +1,43 @@
+//DAY 28 OF HACKERRANK 30 DAY CHALLENGE: REGEX, PATTERNS, AND INTRO TO DATABASES
+
+import java.util.*;
+
+public class Day28 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        String[] inputs = new String[n];
+        String[] names = new String[n];
+        ArrayList<String> outputs = new ArrayList<String>();
+
+        scan.nextLine();
+        for (int x = 0; x < n; x++) {
+            inputs[x] = scan.nextLine();
+            String[] temp = inputs[x].split(" ");
+            names[x] = temp[0];
+
+            if (inputs[x].contains("@gmail.com")) {
+                outputs.add(names[x]);
+            }
+        }
+
+        for (int y = 0; y < outputs.size()-1; y++) {
+            int minIndex = y;
+            for (int z = y+1; z < outputs.size(); z++) {
+                if (outputs.get(minIndex).compareTo(outputs.get(z)) > 0) {
+                    minIndex = z;
+                }
+            }
+            if (minIndex != y) {
+                String temp = outputs.get(y);
+                outputs.set(y,outputs.get(minIndex));
+                outputs.set(minIndex,temp);
+            }
+        }
+
+        for (int i = 0; i < outputs.size(); i++) {
+            System.out.println(outputs.get(i));
+        }
+    }
+}
