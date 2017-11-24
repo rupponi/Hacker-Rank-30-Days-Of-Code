cf88c75a8c4b07b8f51706cc26d66c2846503c65
Rohan Upponi <rponi70@gmail.com>
2017-11-23 17:20:06 -0700

    Pushing Day 27: Testing (C++)

---
+++ CPP-Source/Day27.cpp
@@ -0,0 +1,20 @@
+//DAY 27 OF HACKERRANK 30 DAY CHALLENGE: TESTING (C++)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << 5 << endl;
+	cout << 5 << " " << 4 << endl;
+	cout << 5 << " " << 4 << " " << 3 << " " << -1 << " " << 0 << endl;
+	cout << 4 << " " << 2 << endl;
+	cout << 2 << " " << 3 << " " << 0 << " " << -1 << endl;
+	cout << 5 << " " << 2 << endl;
+	cout << 4 << " " << 2 << " " << 3 << " " << 0 << " " << 1 << endl;
+	cout << 5 << " " << 1 << endl;
+	cout << 5 << " " << 3 << " " << 2 << " " << 9 << " " << 0 << endl;
+	cout << 3 << " " << 3 << endl;
+	cout << 0 << " " << -5 << " " << 1 << endl;
+
+	return 0;
+}
